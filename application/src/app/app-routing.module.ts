1e81fd6021cac0523facedd290bcc95ceb039dde
Tanya <tanya_tanios@live.com>
2021-07-13 19:32:21 +0300

    Creating sidenav , Header

--- application/src/app/app-routing.module.ts
+++ application/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CreditDecisionComponent} from "./credit-decision/credit-decision.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'credit-decision', component: CreditDecisionComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreditDecisionComponent } from './credit-decision/credit-decision.component';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatIconModule} from "@angular/material/icon";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import { SideNavbarComponent } from './credit-decision/side-navbar/side-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreditDecisionComponent,
+    SideNavbarComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSidenavModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatButtonModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
